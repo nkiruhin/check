bbbbbe5cb5a2cf4d50fa57d2607e08ff75abd905
Николай Кирюхин <nkiruhin@yandex.ru>
2019-07-07 13:26:06 +0400

    fix

--- check/check-front/src/App.tsx
+++ check/check-front/src/App.tsx
@@ -3,12 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import { Profile } from './Componet/Profile'
 import { AppSidebar } from './Componet/AppSidebar';
+import { HeadMenu } from './Componet/HeadMenu';
+import { Main } from './Componet/Main';
+import { Configuration } from './Componet/Configuration';
 
 const App: React.FC = () => {
     initializeIcons(/* optional base url */);
     return (
+        <Fabric>
         <div className="App">
             <BrowserRouter>
                 <div className="header">
---
+++ check/check-front/src/Componet/Configuration.tsx
@@ -0,0 +1,6 @@
+﻿import * as React from 'react'
+
+const Configuration: React.FC = () => (
+    <h1>{"Главная"}</h1>
+)
+export { Configuration }
