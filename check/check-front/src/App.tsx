0d6892e03f9d9d1ed72593cdcbafece86c8118bf
Николай Кирюхин <nkiruhin@yandex.ru>
2019-07-07 13:31:44 +0400

    фиксирую->зафиксировал

--- check/check-front/src/App.tsx
+++ check/check-front/src/App.tsx
@@ -2,32 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Fabric } from 'office-ui-fabric-react'
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
-import { Profile } from './Componet/Profile'
 import { AppSidebar } from './Componet/AppSidebar';
 
 const App: React.FC = () => {
     initializeIcons(/* optional base url */);
     return (
+    <Fabric>
         <div className="App">
             <BrowserRouter>
                 <div className="header">
-                    <HeadMenu />
+
                 </div>
                 <div className="body">
-                    <div className="sidebar"><AppSidebar /></div>
+                    <div className="sidebar">
+                        <AppSidebar />
+                    </div>
                     <div className="content">
-                        <Switch>
-                            <Route exact={true} path='/' component={Main} />
-                            <Route path='/profile' component={Profile} />
-                            <Route path='/configuration' component={Configuration} />
-                        </Switch>
+                        
                     </div>
                 </div>
             </BrowserRouter>
             <div className="footer" />
         </div>
-    </Fabric >
+    </Fabric>
   );
 }
 
---
+++ check/check-front/src/Componet/Settings.tsx
@@ -0,0 +1,6 @@
+﻿import * as React from 'react'
+
+const Settings: React.FC = () => (
+    <h1>{"Настройки уведомлений"}</h1>
+)
+export { Settings }
